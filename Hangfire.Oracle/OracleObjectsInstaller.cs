c4b895d8fd5f5ce4cdd17799bdafb099dcfb1bde
Scheffer <vscheffer@narwalsistemas.com.br>
2019-08-06 16:19:00 -0300

    comit inicial

---
+++ Hangfire.Oracle/OracleObjectsInstaller.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Reflection;
+using Dapper;
+using Hangfire.Logging;
+
+namespace Hangfire.Oracle
+{
+    public static class OracleObjectsInstaller
+    {
+        private static readonly ILog Log = LogProvider.GetLogger(typeof(OracleStorage));
+        public static void Install(IDbConnection connection, string schemaName)
+        {
+            if (connection == null) throw new ArgumentNullException(nameof(connection));
+
+            if (TablesExists(connection, schemaName))
+            {
+                Log.Info("DB tables already exist. Exit install");
+                return;
+            }
+
+            Log.Info("Start installing Hangfire SQL objects...");
+
+            var script = GetStringResource("Hangfire.Oracle.Core.Install.sql");
+
+            // TODO: Still problems on script run
+            connection.Execute(script);
+
+            Log.Info("Hangfire SQL objects installed.");
+        }
+
+        private static bool TablesExists(IDbConnection connection, string schemaName)
+        {
+            return connection.ExecuteScalar<string>($@"
+   SELECT TABLE_NAME
+     FROM all_tables
+    WHERE OWNER = '{schemaName}' AND TABLE_NAME LIKE 'HF_%'
+ ORDER BY TABLE_NAME
+") != null;
+        }
+
+        private static string GetStringResource(string resourceName)
+        {
+#if NET45
+            var assembly = typeof(OracleObjectsInstaller).Assembly;
+#else
+            var assembly = typeof(OracleObjectsInstaller).GetTypeInfo().Assembly;
+#endif
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                {
+                    throw new InvalidOperationException($"Requested resource `{resourceName}` was not found in the assembly `{assembly}`.");
+                }
+
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
